504189815bc5bb5dba7e1accd9507b9050dcd931
Antônio Rocha <antoniojunior159@gmail.com>
2024-08-29 20:59:36 -0300

    [Back-end][Catalog.API] - Create Abstraction on MediatR for CQRS - Command and Query separation

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -2,8 +2,7 @@
 
 namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-    : IRequest<CreateProductResult>;
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
     internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
