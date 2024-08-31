1297562e60f08600ff758727ff9b8ea6bc7c44bf
Antônio Rocha <antoniojunior159@gmail.com>
2024-08-30 22:21:30 -0300

    [Back-end][Catalog.API] - Develop Create Product Endpoint with Minimal Apis and Carter

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,9 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+public record CreateProductResponse(Guid id);
+
+public class CreateProductEndpoint
 {
-    public class CreateProductEndpoint
-    {
-    }
+
 }
