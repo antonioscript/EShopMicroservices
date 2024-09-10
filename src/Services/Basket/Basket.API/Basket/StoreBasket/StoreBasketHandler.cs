7c84789fb6f1ddbca3bd4cc77d6337ae476b054d
Antônio Rocha <antoniojunior159@gmail.com>
2024-09-10 01:05:01 -0300

    [Back-end][Basket.API] - Develop StoreBasket methods use cases

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+//public record StoreBasketResult(string UserName);
+public record StoreBasketResult(bool IsSuccess);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class StoreBasketCommandHandler
+    (/*IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto*/)
+    : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+        return new StoreBasketResult(true);
+    }
+    
+}
