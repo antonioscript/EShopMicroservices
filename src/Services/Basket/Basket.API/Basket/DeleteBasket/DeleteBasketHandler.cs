4fcf28a349d3b19330c204e3cfba4273b2ab16a5
Antônio Rocha <antoniojunior159@gmail.com>
2024-09-10 01:08:12 -0300

    [Back-end][Basket.API] - Develop DeleteBasket methos use cases

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
+{
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class DeleteBasketCommandHandler(/*IBasketRepository repository*/)
+    : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        // TODO: delete basket from database and cache       
+
+        return new DeleteBasketResult(true);
+    }
+}
