47c30f304a4e479cb3c82cbd9b032dc0e1fb2860
Antônio Rocha <antoniojunior159@gmail.com>
2024-10-01 20:19:47 -0300

    [Back-end][Discount.API] - Develop DiscountService class to Implement Grpc Proto Service Methods

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,8 +1,15 @@
 
+using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(opts =>
+        opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
+
 builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
@@ -11,7 +18,10 @@ if (app.Environment.IsDevelopment())
 
     app.MapGrpcReflectionService();
 
+app.UseMigration();
 // Configure the HTTP request pipeline.
+app.MapGrpcService<DiscountService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
