c9186c3a57c87bd2d3f29255d044b922136abdc4
Antônio Rocha <antoniojunior159@gmail.com>
2024-09-30 22:19:30 -0300

    [Back-end][Discount.API] - Create Initial Solution for the APi Discount

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,19 @@
+using Discount.Grpc.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+
+    app.MapGrpcReflectionService();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
